13ee2034424276c2c490bce41cda6fd528e7b32e
Kyujin Kang <digiry@gmail.com>
2022-10-05 23:54:18 +0900

    add memo I learned 2022-10-05

---
+++ 03_advanced/scripts/module.js
@@ -0,0 +1,9 @@
+export function log(message) {
+    console.log(message);
+}
+
+export function error(message) {
+    console.error(message);
+}
+
+export var PI = 3.14;
---
+++ 03_advanced/scripts/ref.js
@@ -0,0 +1,5 @@
+import {
+    log
+} from "./module.js";
+
+log("REF에서 출력하는 메시지");
